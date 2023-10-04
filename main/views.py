0e22f63c433d99a65db32432d312290bb03bed91
nandapratama21 <nanda241power@gmail.com>
2023-10-04 16:18:42 +0700

    Tutorial 4 selesai

--- main/views.py
+++ main/views.py
@@ -14,6 +14,8 @@ from django.urls import reverse
 from .models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotFound
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -117,3 +119,23 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
